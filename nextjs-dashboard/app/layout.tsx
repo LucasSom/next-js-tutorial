9e330d5fd402daf0d682aa925620e13c419162e2
LucasSom <lsomacal@gmail.com>
2025-06-02 17:29:17 -0300

    Layout del dashboard

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
-export default function RootLayout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
